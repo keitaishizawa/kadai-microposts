361184e4f9c29fe67251ef99e6e4c8b0597bea44
keitaishizawa <kedam.ishizawa@gmail.com>
2017-10-11 07:11:07 +0000

    post

---
+++ app/Http/Controllers/MicropostsController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class MicropostsController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'content' => 'required|max:255',    
+        ]);
+        
+        $request->user()->microposts()->create([
+            'content' => $request->content,
+        ]);
+        
+        return redirect('/');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = User::find($id);
+        $micropost = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+        $count_microposts = $user->microposts()->count();
+        
+        $data = [
+            'user' => $user,
+            'microposts' => $microposts,
+        ];
+        
+        $data += $this->counts($user);
+        
+        return view('users.show', $data);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $micropost = \App\Micropost::find($id);
+        
+        if (\Auth::user()->id === $micropost->user_id) {
+            $micropost->delete();
+        }
+        
+        return redirect()->back();
+    }
+}
