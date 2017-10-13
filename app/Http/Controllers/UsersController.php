8d305733eda1c837bb2c7e9c487cc8bc838590d1
keitaishizawa <kedam.ishizawa@gmail.com>
2017-10-13 06:50:22 +0000

    follow, unfollow

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,8 +16,7 @@ class UsersController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
+    public function index() {
         $users = User::paginate(10);
         
         return view('users.index', [
@@ -31,13 +30,46 @@ class UsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
-    {
+    public function show($id) {
         $user = User::find($id);
+        $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+        $count_microposts = $user->microposts()->count();
         
-        return view('users.show', [
-            'user' => $user,    
-        ]);
+        $data = [
+            'user' => $user,
+            'microposts' => $microposts,
+        ];
+        
+        $data += $this->counts($user);
+        
+        return view('users.show', $data);
     }
     
+    public function followings($id) {
+        $user = User::find($id);
+        $followings = $user->followings()->paginate(10);
+        
+        $data = [
+            'user' => $user,
+            'users' => $followings,
+        ];
+        
+        $data += $this->counts($user);
+        
+        return view('users.followings', $data);
+    }
+    
+    public function followers($id) {
+        $user = User::find($id);
+        $followers = $user->followers()->paginate(10);
+        
+        $data = [
+            'user' => $user,
+            'users' => $followers,
+        ];
+        
+        $data += $this->counts($user);
+        
+        return view('users.followers', $data);
+    }
 }
--- app/User.php
+++ app/User.php
@@ -40,4 +40,60 @@ class User extends Model implements AuthenticatableContract,
     public function microposts() {
         return $this->hasMany(Micropost::class);
     }
+    
+    /**
+     * フォローしているユーザ達を取得
+     */
+    public function followings() {
+        return $this->belongsToMany(User::class, 'user_follow', 'user_id', 'follow_id')->withTimestamps(); 
+    }
+    
+    public function followers() {
+        return $this->belongsToMany(User::class, 'user_follow', 'follow_id', 'user_id')->withTimestamps();
+    }
+    
+    /**
+     * フォローする
+     */
+    public function follow($userId) {
+        // 既にフォローしているか確認
+        $exist = $this->is_following($userId);
+        // 自分自身でないかの確認
+        $its_me = $this->id == $userId;
+        
+        if($exist || $its_me) {
+            // 既にフォローしていれば何もしない
+            return false;
+        } else {
+            // 未フォローであればフォローする
+            $this->followings()->attach($userId);
+            return true;
+        }
+    }
+    
+    /**
+     * フォローを外す
+     */
+    public function unfollow($userId) {
+        // 既にフォローしているか確認
+        $exist = $this->is_following($userId);
+        // 自分自身でないかの確認
+        $its_me = $this->id == $userId;
+        
+        if($exist && !$its_me) {
+            // 既にフォローしていればフォローを外す
+            $this->followings()->detach($userId);
+            return true;
+        } else {
+            // 未フォローであれば何もしない
+            return false;
+        }
+    }
+    
+    /**
+     * フォローしているか確認
+     */
+    public function is_following($userId) {
+        return $this->followings()->where('follow_id', $userId)->exists();
+    }
 }
